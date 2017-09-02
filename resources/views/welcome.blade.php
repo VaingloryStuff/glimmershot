c7f8990eededcb2e58c8e7af56ece6f629f8f779
agangofkittens <agangofkittens@gmail.com>
2017-09-02 19:02:44 +0200

    Initial commit.

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,126 @@
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }} {{ app()->version() }}</title>
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <!-- Styles -->
+    <link href="{{ mix('/css/app.css') }}" rel="stylesheet">
+
+</head>
+<body>
+  <section class="hero is-primary is-fullheight header-image">
+    <div class="hero-body">
+      <div class="container has-text-centered">
+        <div class="column is-6 is-offset-3">
+          <div class="field has-addons">
+              <div class="control is-expanded">
+                <input class="input" type="text" placeholder="Player name">
+            </div>
+            <div class="control">
+                <div class="select">
+                    <select>
+                        <option>NA</option>
+                        <option>EU</option>
+                        <option>SA</option>
+                        <option>EA</option>
+                        <option>SG</option>
+                        <option>CN</option>
+                    </select>
+                </div>
+            </div>
+            <div class="control">
+                <a class="button is-primary">
+                    <span class="icon">
+                        <i class="fa fa-search"></i>
+                    </span>
+                </a>
+            </div>
+        </div>
+    </div>
+<!--         <h1 class="title is-2">
+          Hero Heading
+        </h1>
+        <h2 class="subtitle is-5">
+          A hero description could go here.
+        </h2>
+        <p>
+          <a class="button is-outlined">
+            <span class="icon">
+              <i class="fa fa-download"></i> 
+            </span>
+            <span>
+              Download
+            </span>
+          </a>
+        </p> -->
+      </div>
+    </div>
+  </section>
+  <div class="hero-cta">
+    <nav class="level">
+      <div class="level-item has-text-centered">
+        <p class="title">Do something. <a class="button is-primary"><span class="icon"><i class="fa fa-download"></i></span><span>Intent</span></a></p>
+      </div>
+    </nav>
+  </div>
+  <div class="section main">
+    <div class="container">
+      <div class="columns">
+        <div class="column is-4">
+          <div class="panel">
+            <div class="panel-block section">
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean efficitur sit amet massa fringilla egestas. Nullam condimentum luctus turpis.</p>
+            </div>
+          </div>
+        </div>
+        <div class="column is-4">
+          <div class="panel">
+            <div class="panel-block section">
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean efficitur sit amet massa fringilla egestas. Nullam condimentum luctus turpis.</p>
+            </div>
+          </div>
+
+        </div>
+        <div class="column is-4">
+          <div class="panel">
+            <div class="panel-block section">
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean efficitur sit amet massa fringilla egestas. Nullam condimentum luctus turpis.</p>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
+  </div>
+  <footer class="footer">
+    <div class="container">
+      <div class="content has-text-centered">
+        <p>
+          <strong>Glimmershot</strong> by <a href="http://github.com/agangofkittens">agangofkittens</a>. The source code is licensed under
+          <a href="http://opensource.org/licenses/mit-license.php">MIT</a>.
+        </p>
+        <p>
+          <i>Glimmershot is not affiliated, endorsed, sponsored or approved by Super Evil Megacorp and its affiliates.</i>
+        </p>
+        <p>
+          <a class="icon" href="https://github.com/jgthms/bulma">
+            <i class="fa fa-github"></i>
+          </a>
+        </p>
+      </div>
+    </div>
+  </footer>
+    <!-- Scripts -->
+    <script src="{{ mix('/js/app.js') }}"></script>
+
+</body>
+</html>
