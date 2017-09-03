eb74b4ca432782133ab52e0c96d075bfbbce7c68
agangofkittens <agangofkittens@gmail.com>
2017-09-04 01:14:05 +0200

    Add portraits, add player page.

---
+++ app/Http/Controllers/BaseController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Services\GetPlayerService;
+
+class BaseController extends Controller
+{
+    /**
+     * Player instance
+     *
+     * @var GetPlayerService
+     */
+    protected $player;
+
+    public function __construct(GetPlayerService $player)
+    {
+        $this->getPlayerService = $player;
+    }
+
+    /**
+     * Perform the search in the /player/region/player_name.
+     *
+     * @param $region
+     * @param $player_name
+     * @return $this
+     */
+    public function index($region, $player_name)
+    {
+        $player = $this->getPlayerService->search($region, $player_name);
+
+        return view('player', ['player' => $player]);
+    }
+
+     /**
+     * Perform a search from the homepage.
+     *
+     * @param Request $request
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function search(Request $request) {
+
+        return redirect()->route('player', [
+            'region' => $request->region,
+            'player_name' => $request->player_name
+        ]);
+    }
+}
--- resources/views/player.blade.php
+++ resources/views/player.blade.php
@@ -8,13 +8,63 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }} {{ app()->version() }}
+    <title>Glimmershot | {{ $player['name'] }}
     </title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <!-- Styles -->
     <link href="{{ mix('/css/app.css') }}" rel="stylesheet">
 
+    <style>
+      #IMG_1 {
+        display: block;
+        margin: 0 auto;
+        box-shadow: rgba(0, 0, 0, 0.5) 0px 0px 10px 3px;
+        box-sizing: border-box;
+        color: rgb(62, 58, 71);
+        height: 120px;
+        text-align: center;
+        text-decoration: none solid rgb(62, 58, 71);
+        width: 120px;
+        column-rule-color: rgb(62, 58, 71);
+        perspective-origin: 60px 60px;
+        transform-origin: 60px 60px;
+        caret-color: rgb(62, 58, 71);
+        border: 0px none rgb(62, 58, 71);
+        border-radius: 100% 100% 100% 100%;
+        font: normal normal normal normal 14px / normal "Source Sans Pro", Calibri, sans-serif;
+        outline: rgb(62, 58, 71) none 0px;
+      }
+      .header-image {
+        background-image: linear-gradient(rgba(0, 0, 0, 0.49),rgba(0, 0, 0, 0.89)),url('/images/backgrounds/Kestrel_Winter_War_R.jpg');
+        background-position: top center; 
+        -webkit-background-size: cover;
+        -moz-background-size: cover;
+        background-size: cover;
+        -o-background-size: cover;
+      }
+
+    </style>
+
   </head>
   <body>
+
+  <section class="hero is-primary header-image">
+  <div class="hero-body">
+    <div class="container">
+      <div class="columns is-vcentered is-mobile">
+        <div class="column is-half is-offset-one-quarter">
+            <img src="{{ asset('images/portraits/Vox_Portrait.png') }}" alt="{{ $player['picks']['0']['actor'] }}" id="IMG_1">
+        </div>
+    </div>
+  </div>
+</div>
+</section>
+
+  <img src="{{ asset('images/portraits/Vox_Portrait.png') }}" alt="{{ $player['picks']['0']['actor'] }}" id="IMG_1">
+
+<p>
+      {{ $player['picks']['0']['actor'] }}
+      {{ $player['picks']['0']['hero_pick'] }}
+</p>
   </body>
-  </html>
+</html>
